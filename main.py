3f3146131e0c0fcf859fcccc0e0a6bc21183edb1
Pereira-Luc <luc.pereira.cardoso@gmail.com>
2023-11-17 00:35:46 +0100

    example for data splitter

--- main.py
+++ main.py
@@ -12,5 +12,15 @@ if __name__ == "__main__":
     path_TSLA = "data/TSLA_index.csv"
 
     file_paths = [path_apple, path_TSLA,path_apple, path_TSLA,path_apple, path_TSLA]
-    read_multiple_files_parallel(file_paths)
+    data_list_array = read_multiple_files_parallel(file_paths)
+
+
+    if rank == 0:
+        apple_data = data_list_array[0][0]
+
+        print("Data: ", len(apple_data))
+        train, test = get_train_test_data(apple_data, 0.8)
+
+        print("Train: ", len(train))
+        print("Test: ", len(test))
 
